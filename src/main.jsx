74ccd39d8c855897c64d6017ee311021d8979f7c
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-28 03:05:19 +0800

    footer has been added

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import { router } from './Routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='max-w-screen-xl mx-auto'>
     <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
