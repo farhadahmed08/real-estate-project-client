eb2b446e446e4fd2da533260b1163bec3adbd25f
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-28 03:16:29 +0800

    firebase initialize has been done

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import NavBar from "../Pages/Shared/NavBar/NavBar";
 
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+
+
+const NavBar = () => {
+    return (
+        <div>
+           <h2>this is navbar</h2> 
+        </div>
+    );
+};
+
+export default NavBar;
