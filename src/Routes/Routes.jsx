75aa7eff4fa2a972c64cb4365adddff4a1cc3af7
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-28 02:55:27 +0800

    error page has been added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,11 +4,13 @@ import {
   } from "react-router-dom";
 import Main from "../LayOut/Main";
 import Home from "../Pages/Home/Home/Home";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
  export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <ErrorPage />,
       children: [
         {
           path: "/",
