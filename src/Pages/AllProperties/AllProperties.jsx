09aa834af5511dc44fd0584cbb5828675dce986b
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-29 15:42:49 +0800

    all properties page added

--- src/Pages/AllProperties/AllProperties.jsx
+++ src/Pages/AllProperties/AllProperties.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import Properties from "./Properties";
 
 
 const AllProperties = () => {
+
+
+    const [allProperties,setAllProperties] = useState([])
+
+    useEffect(()=>{
+        fetch('allProperties.json')
+        .then(res=>res.json()
+        .then(data =>{
+           setAllProperties(data)
+        }))
+    },[])
+
+
+
     return (
-        <div>
-            <h2>all properties</h2>
+        <div className="grid md:grid-cols-3 gap-3 grid-cols-1">
+            {
+                allProperties.map(properties=><Properties key={properties.id} properties={properties}></Properties>)
+            }
         </div>
     );
 };
---
+++ src/Pages/AllProperties/Properties.jsx
@@ -0,0 +1,49 @@
+
+
+const Properties = ({properties}) => {
+    const {image,title,location,agentName,agentImage,verificationStatus,priceRange} = properties
+    return (
+        
+        
+        <div >
+     <div className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+  <div className="relative m-0 overflow-hidden text-gray-700 bg-transparent rounded-none shadow-none bg-clip-border ">
+    <img
+      src={image}
+      alt="ui/ux review check"
+    />
+  </div>
+  <div className="p-6">
+    <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+      {title}
+    </h4>
+    <p className="block mt-3 font-sans text-xl antialiased font-normal leading-relaxed text-gray-700">
+      {location}
+    </p>
+    <p className="block mt-3 font-sans text-xl antialiased font-normal leading-relaxed text-gray-700">
+      Status: <span className="text-blue-800">{verificationStatus}</span>
+    </p>
+  </div>
+  <div className="flex items-center justify-between p-6 ">
+    <div className="flex items-center -space-x-3 gap-5">
+      <img
+        alt="natali craig"
+        src={agentImage}
+        className="relative inline-block object-cover object-center border-2 border-white rounded-full h-9 w-9 hover:z-10"
+        data-tooltip-target="author-1"
+      />
+     <p>{agentName}</p>
+      
+    
+    </div>
+    <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+      ${priceRange}
+    </p>
+  </div>
+<button className="btn btn-primary ">Details</button>
+</div>
+    </div>
+    );
+};
+
+export default Properties;
